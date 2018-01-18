9c0e0e3f5f235219ed7bbd6b3be3d81ee82334f4
Jared Brown <jb_90210@hotmail.com>
2018-01-18 04:27:36 +0000

    Integrated new gem into rails app for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+  
+  before_action :set_copyright
+  
+  def set_copyright
+    @copyright = JaredBrownViewTool::Renderer.copyright 'Jared Brown', 'Allrights reserved'
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    JaredBrownViewTool::Renderer.copyright 'Jared Brown', 'Allrights reserved'
+  end
 end
